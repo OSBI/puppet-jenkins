5f870dbafadd14f9fb987bae1e7a9196f5b7990a
Tom Barber <tom@analytical-labs.com>
2011-10-13 07:58:04 +0100

    alter paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,14 +16,13 @@ apache::vhost {"ci.analytical-labs.com":
 tomcat::instance {"jenkins":
   ensure      => present,
   ajp_port    => "8009",
-  http_port   => "8080",
 }
 
 apache::proxypass {"jenkins":
   ensure   => present,
   location => "/jenkins",
   vhost    => "ci.analytical-labs.com",
-  url      => "ajp://localhost:8009",
+  url      => "ajp://localhost:8009/jenkins",
 }
 
 }
